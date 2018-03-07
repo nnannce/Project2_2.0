39a1289cd7d3cb49d876b9fa7655f16bc8afdad6
Austin Hammontree <30849233+AJHammontree@users.noreply.github.com>
2018-03-06 20:17:15 -0600

    flight date time

--- business/assets/js/flight.js
+++ business/assets/js/flight.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+  $("#preloader").hide();
+});
 
 var From = "";
 var To = "";
@@ -18,7 +21,8 @@ function runQuery() {
 
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    dataType: 'json'
   }).done(function(Data) {
 
     // Logging the URL so we have access to it for troubleshooting
@@ -26,39 +30,42 @@ function runQuery() {
     console.log("URL: " + queryURL);
     console.log("------------------------------------");
 
+    console.log(moment('2014-08-20 15:30:00').format('MM/DD/YYYY h:mm a'));
+    
     // Log the Data to console, where it will show up as an object
     console.log(Data);
     console.log("------------------------------------");
+    $("#preloader").hide();
     $("h4").text("Per ticket:" + " " + "$" + Data.results["0"].fare.price_per_adult.total_fare);
     $("h4").append("<br><br>Airline:" + " " + Data.results["0"].itineraries["0"].outbound.flights["0"].marketing_airline);
-    $("h4").append("<br><br>Departs:" + " " + Data.results["0"].itineraries["0"].outbound.flights["0"].departs_at);
-    $("h4").append("<br>Arrives:" + " " + Data.results["0"].itineraries["0"].outbound.flights["0"].arrives_at);
-    $("h4").append("<br><br>Departs:" + " " + Data.results["0"].itineraries["0"].inbound.flights["0"].departs_at);
-    $("h4").append("<br>Arrives:" + " " + Data.results["0"].itineraries["0"].inbound.flights["0"].arrives_at);
+    $("h4").append("<br><br>Departs:" + " " + moment(Data.results["0"].itineraries["0"].outbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h4").append("<br>Arrives:" + " " + moment(Data.results["0"].itineraries["0"].outbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
+    $("h4").append("<br><br>Departs:" + " " + moment(Data.results["0"].itineraries["0"].inbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h4").append("<br>Arrives:" + " " +  moment(Data.results["0"].itineraries["0"].inbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
     $("h4").append("<br><br>Seats Remaining:" + " " + Data.results["0"].itineraries["0"].inbound.flights["0"].booking_info.seats_remaining);
     $("h4").append("<br><br> Total Cost" + " " + "$" + Data.results["0"].fare.total_price);
     $("h5").append("Per ticket:" + " " + "$" + Data.results["1"].fare.price_per_adult.total_fare);
     $("h5").append("<br><br>Airline:" + " " + Data.results["1"].itineraries["0"].outbound.flights["0"].marketing_airline);
-    $("h5").append("<br><br>Departs:" + " " + Data.results["1"].itineraries["0"].outbound.flights["0"].departs_at);
-    $("h5").append("<br>Arrives:" + " " + Data.results["1"].itineraries["0"].outbound.flights["0"].arrives_at);
-    $("h5").append("<br><br>Departs:" + " " + Data.results["1"].itineraries["0"].inbound.flights["0"].departs_at);
-    $("h5").append("<br>Arrives:" + " " + Data.results["1"].itineraries["0"].inbound.flights["0"].arrives_at);
+    $("h5").append("<br><br>Departs:" + " " + moment(Data.results["1"].itineraries["0"].outbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h5").append("<br>Arrives:" + " " +  moment(Data.results["1"].itineraries["0"].outbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
+    $("h5").append("<br><br>Departs:" + " " + moment(Data.results["1"].itineraries["0"].inbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h5").append("<br>Arrives:" + " " +  moment(Data.results["1"].itineraries["0"].inbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
     $("h5").append("<br><br>Seats Remaining:" + " " + Data.results["1"].itineraries["0"].inbound.flights["0"].booking_info.seats_remaining);
     $("h5").append("<br><br> Total Cost" + " " + "$" + Data.results["1"].fare.total_price);
     $("h6").append("Per ticket:" + " " + "$" + Data.results["2"].fare.price_per_adult.total_fare);
     $("h6").append("<br><br>Airline:" + " " + Data.results["2"].itineraries["0"].outbound.flights["0"].marketing_airline);
-    $("h6").append("<br><br>Departs:" + " " + Data.results["2"].itineraries["0"].outbound.flights["0"].departs_at);
-    $("h6").append("<br>Arrives:" + " " + Data.results["2"].itineraries["0"].outbound.flights["0"].arrives_at);
-    $("h6").append("<br><br>Departs:" + " " + Data.results["2"].itineraries["0"].inbound.flights["0"].departs_at);
-    $("h6").append("<br>Arrives:" + " " + Data.results["2"].itineraries["0"].inbound.flights["0"].arrives_at);
+    $("h6").append("<br><br>Departs:" + " " + moment(Data.results["2"].itineraries["0"].outbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h6").append("<br>Arrives:" + " " +  moment(Data.results["2"].itineraries["0"].outbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
+    $("h6").append("<br><br>Departs:" + " " + moment(Data.results["2"].itineraries["0"].inbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h6").append("<br>Arrives:" + " " +  moment(Data.results["2"].itineraries["0"].inbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
     $("h6").append("<br><br>Seats Remaining:" + " " + Data.results["2"].itineraries["0"].inbound.flights["0"].booking_info.seats_remaining);
     $("h6").append("<br><br> Total Cost" + " " + "$" + Data.results["2"].fare.total_price);
     $("h3").append("Per ticket:" + " " + "$" + Data.results["3"].fare.price_per_adult.total_fare);
     $("h3").append("<br><br>Airline:" + " " + Data.results["3"].itineraries["0"].outbound.flights["0"].marketing_airline);
-    $("h3").append("<br><br>Departs:" + " " + Data.results["3"].itineraries["0"].outbound.flights["0"].departs_at);
-    $("h3").append("<br>Arrives:" + " " + Data.results["3"].itineraries["0"].outbound.flights["0"].arrives_at);
-    $("h3").append("<br><br>Departs:" + " " + Data.results["3"].itineraries["0"].inbound.flights["0"].departs_at);
-    $("h3").append("<br>Arrives:" + " " + Data.results["3"].itineraries["0"].inbound.flights["0"].arrives_at);
+    $("h3").append("<br><br>Departs:" + " " + moment(Data.results["3"].itineraries["0"].outbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h3").append("<br>Arrives:" + " " +  moment(Data.results["3"].itineraries["0"].outbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
+    $("h3").append("<br><br>Departs:" + " " + moment(Data.results["3"].itineraries["0"].inbound.flights["0"].departs_at).format('h:mm a MM-DD-YYYY'));
+    $("h3").append("<br>Arrives:" + " " +  moment(Data.results["3"].itineraries["0"].inbound.flights["0"].arrives_at).format('h:mm a MM-DD-YYYY'));
     $("h3").append("<br><br>Seats Remaining:" + " " + Data.results["3"].itineraries["0"].inbound.flights["0"].booking_info.seats_remaining);
     $("h3").append("<br><br> Total Cost" + " " + "$" + Data.results["3"].fare.total_price);
     
@@ -71,7 +78,13 @@ $("#run-search").on("click", function(event) {
 
   event.preventDefault();
 
-  
+  $("h3").empty();
+  $("h4").empty();
+  $("h5").empty();
+  $("h6").empty();
+
+  $("#preloader").show();
+
   From = $("#from").val();
   To = $("#to").val();
   departure_date = $("#departure_date").val();
